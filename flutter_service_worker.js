1ef85ca3a1478f017a97500a0d9ea6d6382652f2
Kingtous <Kingtous@users.noreply.github.com>
2023-12-01 11:01:24 +0000

    deploy: 31e60006d2fe7c249ec78733268aeb1ee08debf9

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,8 +29,8 @@ const RESOURCES = {"icons/Icon-192.png": "9a4640177733c92952f7cf1fadb5fc2c",
 "assets/fonts/MaterialIcons-Regular.otf": "985bb06d70996ec51b0d509f8be6a0e4",
 "assets/NOTICES": "11ebce186d7a06d3db0c662c09c11513",
 "login.json": "2b606600fee0731a48df538a5b7caf9a",
-"index.html": "ab0b6bd68121a163389e395e025a1cde",
-"/": "ab0b6bd68121a163389e395e025a1cde",
+"index.html": "c15a179cd07ebc4e66d1c17b565b1f9c",
+"/": "c15a179cd07ebc4e66d1c17b565b1f9c",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a"};
 // The application shell files that are downloaded before a service worker can
 // start.
